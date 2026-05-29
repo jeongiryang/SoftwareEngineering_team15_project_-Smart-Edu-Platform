79a24004d7942be9d340fe674c5df9405ae1e435
Jungiryang <eryang1118@gmail.com>
2026-05-29 21:56:26 +0900

    [fix] Expo Web API base URL env 반영 수정

--- src/frontend/src/constants/config.js
+++ src/frontend/src/constants/config.js
@@ -1,8 +1,6 @@
 const DEFAULT_API_BASE_URL = 'http://localhost:4000/api';
-const API_BASE_URL_ENV_NAME = 'EXPO_PUBLIC_API_BASE_URL';
 
-const envApiBaseUrl =
-  typeof process !== 'undefined' ? process.env?.[API_BASE_URL_ENV_NAME] : undefined;
+const envApiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 const normalizeApiBaseUrl = (value) => {
   if (!value || typeof value !== 'string') {
@@ -18,4 +16,3 @@ const normalizeApiBaseUrl = (value) => {
 };
 
 export const API_BASE_URL = normalizeApiBaseUrl(envApiBaseUrl);
-export { API_BASE_URL_ENV_NAME };
